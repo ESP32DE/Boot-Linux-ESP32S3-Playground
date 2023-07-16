ad530712abfe7c6aa7564f9bf3fb0c03d42fc1b4
rudi ;-) <ESP32DE@users.noreply.github.com>
2023-07-16 05:23:52 +0200

    init hi.py

---
+++ micropython/public/hi.py
@@ -0,0 +1,13 @@
+# liosti-esp32de-wifis3linux
+# 16. July 2023
+# rudi ;-)
+# just testing
+# - online download
+# - online load
+# 
+import micropython as mpy
+
+mpy.mem_info()
+
+print("just testings things")
+print("greetings fly out to friends all over the globe")
